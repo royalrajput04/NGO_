382caa69b22f349555e6345b49f7a485fd0b8c3c
Ankit Pratap <123142821+royalrajput04@users.noreply.github.com>
2024-06-17 00:06:09 +0530

    Video Modal

--- js/main.js
+++ js/main.js
@@ -73,18 +73,20 @@
     // Modal Video
     $(document).ready(function () {
         var $videoSrc;
+        
         $('.btn-play').click(function () {
             $videoSrc = $(this).data("src");
+            console.log($videoSrc);
         });
-        console.log($videoSrc);
-
+    
         $('#videoModal').on('shown.bs.modal', function (e) {
             $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
+        });
+    
         $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+            // Stop the video when the modal is closed
+            $("#video").attr('src', '');
+        });
     });
     
     
